60a96fdffe37222a7e85d3be1c03fc81f785744d
Marcilio Cobel <marcilio.c.junior@accenture.com>
2019-06-02 22:29:50 -0300

    configure unit tests

--- front-end-interview/src/app/services/github.service.spec.ts
+++ front-end-interview/src/app/services/github.service.spec.ts
@@ -1,12 +1,22 @@
 import { TestBed } from '@angular/core/testing';
-
 import { GithubService } from './github.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+
 
 describe('GithubService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [GithubService]
+  }));
 
   it('should be created', () => {
     const service: GithubService = TestBed.get(GithubService);
     expect(service).toBeTruthy();
+  });
+
+  it('Obetendo Repositórios', () => {
+    const service: GithubService = TestBed.get(GithubService);
+    expect(service.getUserData('marcilio9590')).toBeTruthy();
   });
 });
